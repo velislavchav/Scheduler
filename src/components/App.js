52635ffe7798f99898f629da69f14d48660b0fb8
unknown <velislav.chav@gmail.com>
2022-02-01 20:22:04 +0200

    Added private/general router blocker

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
 
 // Libraries
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
+
+import { getSelectedLanguage, changeLanguage, checkIsLanguageValid } from '../utils/helpers';
+import GeneralRouteOnly from '../utils/route-blocking/GeneralRouteOnly';
+import PrivateRouteOnly from '../utils/route-blocking/PrivateRouteOnly';
 
 // Components
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Home from './Home/Home';
 import Activities from './Activities/Activities';
-import AddActivity from './Activities/AddActivity/AddActivity'
-import { getSelectedLanguage, changeLanguage, checkIsLanguageValid } from '../utils/helpers';
+import AddActivity from './Activities/AddActivity/AddActivity';
+import Login from './User/Login/Login';
 
 function App() {
   const [language, setLanguage] = useState("BG");
@@ -35,7 +39,15 @@ function App() {
       <Routes >
         <Route path="/" element={<Home language={language} />} />
         <Route path="/services/:category/:subcategory" element={<Activities language={language} />} />
-        <Route path="/services/add" element={<AddActivity language={language} />} />
+        {/* PrivateRouteOnly */}
+        <Route element={<PrivateRouteOnly />} >
+          <Route path="/services/add" element={<AddActivity language={language} />} />
+        </Route>
+        {/* GeneralRouteOnly */}
+        <Route element={<GeneralRouteOnly />} >
+          <Route path="/user/login" element={<Login language={language} />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes >
       <Footer />
     </>
